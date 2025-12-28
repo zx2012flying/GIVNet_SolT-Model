bb36b52b06a1c989f1dc1bd7089e95b286b857d4
Xiang Zhang <42462557+zx2012flying@users.noreply.github.com>
2025-12-28 20:20:02 +0800

    Delete Final model for prediction directory

--- Final model for prediction/layers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .MPNN import MPNN
-from .schnet import SchNet
-from .auto_encoder import VariationalAutoEncoder
-from .equivariant_scorenetwork import EquivariantScoreNetwork, EquiLayer
-from .AttentiveFP import AttentiveFP
-from .moleculeSDE import SDEModel2Dto3D_01, SDEModel2Dto3D_02
